76680b0a8fff8d8dd0a41f9b3105c346e4862fa2
zoltan-antal <zoltan_antal@outlook.com>
2023-09-08 16:14:31 +0100

    Add restore logic

--- vivaldi_restore_urls.py
+++ vivaldi_restore_urls.py
@@ -12,7 +12,31 @@ fhand.close()
 
 urlList = []
 
+while True:
+    if contents.find("urlForThumbnail") == -1:
+        break
+
+    start = contents.find("\"urlForThumbnail\"")
+    start += 19
+
+    contents = contents[start:]
+
+    end = contents.find("\"vivaldi_tab_muted\"")
+    if (end > 5000 or end == -1):
+        end = contents.find("\"thumbnail\"")
+    if (end > 5000 or end == -1):
+        continue
+    end -= 2
+
+    print(contents[:end])
+
+    urlList.append(contents[:end])
+    urlList.append("\n")
+
+    contents = contents[end:]
 
 fhand = open("./decoded/" + file_name + ".txt", "w")
 fhand.writelines(urlList)
 fhand.close()
+
+print("Succesfully executed. Find decoded files with URLs in dist folder.")
