95ee7c73370eca073ac053b2f6b7c46bd2c770af
zoltan-antal <zoltan_antal@outlook.com>
2023-09-08 16:55:43 +0100

    Create merging utility

---
+++ merge_output.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+file_names = sorted(os.listdir("./dist"))
+file_names = list(filter(lambda file_name: file_name.find("merged") == -1, file_names))
+print(file_names)
+
+urls = set()
+
+for file_name in file_names:
+    with open("./dist/" + file_name, "r") as fhand:
+        for line in fhand:
+            urls.add(line)
+
+fhand = open("./dist/" + "merged-" + str(time.time()) + ".txt", "w")
+fhand.writelines(urls)
+fhand.close()
+
+print("Succesfully executed. Find merged with all URLs in dist folder.")
